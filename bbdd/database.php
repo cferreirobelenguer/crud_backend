5ffd475fd3b3442958256a3e290b1c83207cb1ec
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2023-09-04 18:33:37 +0200

    composer and .env

--- bbdd/database.php
+++ bbdd/database.php
@@ -1,11 +1,18 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
+use Dotenv\Dotenv;
+
+// Load environment variables from .env
+$dotenv = Dotenv::createImmutable(__DIR__ . '/..'); 
+$dotenv->load();
+
 
 //database conection
 function databaseConection() {
-    $servername = 'localhost';
-    $database = 'productos';
-    $username = 'root';
-    $password = '';
+    $servername = $_ENV['DB_HOST'];
+    $database = $_ENV['DB_NAME'];
+    $username = $_ENV['DB_USER'];
+    $password = $_ENV['DB_PASS'];
 
     try {
         $dsn = "mysql:host=$servername;dbname=$database;charset=utf8mb4";
