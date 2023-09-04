c69ae5e7755b1f8f9899e2490e2e04161015b415
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2023-09-04 17:46:29 +0200

    documentation delete

--- controller/delete.php
+++ controller/delete.php
@@ -1,19 +1,21 @@
 <?php
 //Delete product
 function delete() {
+    //CORS
     header("Access-Control-Allow-Origin: http://localhost:4200");
     header("Access-Control-Allow-Methods: DELETE");
+    //database connection
     include_once '../bbdd/database.php';
-
     $metodo = $_SERVER["REQUEST_METHOD"];
     if ($metodo != "DELETE" && $metodo != "OPTIONS") {
         exit("Solo se permite método DELETE");
     }
 
+    //id is empty
     if (empty($_GET["idProduct"])) {
         exit("No hay id de producto para eliminar");
     }
-
+    //we delete information with id
     $idProduct= $_GET['idProduct'];
 
     $db = databaseConection();
