49c6c0c7f38b85b985b9361880ffbce761005e46
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2023-09-04 19:26:32 +0200

    create user

--- controller/create.php
+++ controller/create.php
@@ -1,4 +1,32 @@
 <?php
 
+//Create products
+function createProducts() {
+    //CORS, only localhost:4200 and method POST
+    header("Access-Control-Allow-Origin: http://localhost:4200");
+    header("Access-Control-Allow-Methods: POST");
+    //database connection
+    include_once '../bbdd/database.php';
+    $metodo = $_SERVER["REQUEST_METHOD"];
+    if ($metodo != "POST" && $metodo != "OPTIONS") {
+        exit("Solo se permite método POST");
+    }
+
+    //The data are empty
+    if (empty($_POST["description"]) || empty($_POST["price"]) || empty($_POST["img"])) {
+        exit("No hay datos");
+    }
+    //information of product
+    $description= $_POST['description'];
+    $price = $_POST['price'];
+    $img = $_POST['img'];
+
+    $db = databaseConection();
+    $sentencia = $db->prepare("INSERT INTO stock (descripcion, precio, img) VALUES (?, ?, ?)");
+    $resultado = $sentencia->execute([$description, $price, $img]);
+    echo json_encode($resultado);
+}
+
+createProducts();
 
 ?>
--- controller/update.php
+++ controller/update.php
@@ -1,4 +1,17 @@
 <?php
+//Create products
+function updateProducts() {
+    //CORS, only localhost:4200 and method POST
+    header("Access-Control-Allow-Origin: http://localhost:4200");
+    header("Access-Control-Allow-Methods: POST");
+    //database connection
+    include_once '../bbdd/database.php';
+    $metodo = $_SERVER["REQUEST_METHOD"];
+    if ($metodo != "POST" && $metodo != "OPTIONS") {
+        exit("Solo se permite método POST");
+    }
+}
 
+updateProducts();
 
 ?>
