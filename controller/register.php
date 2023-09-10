926df62f3ae9dd2bec8494d45ae609d12b22b7ce
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2023-09-10 20:34:44 +0200

    register data json

--- controller/register.php
+++ controller/register.php
@@ -6,6 +6,7 @@ function registerUser() {
     header("Access-Control-Allow-Methods: POST");
     header("Access-Control-Allow-Headers: Content-Type");
 
+
      // Database connection
     include_once '../bbdd/database.php';
     $db = databaseConection();
@@ -15,23 +16,32 @@ function registerUser() {
         exit("Solo se permite método POST");
     }
 
-    if (!isset($_POST['name'], $_POST['username'], $_POST['password'], $_POST['email'])) {
-        //No data in body
-        exit('Not found data');
+    // json file
+    $inputJSON = file_get_contents('php://input');
+
+    $data = json_decode($inputJSON, true);
+
+    if ($data === null) {
+        echo json_encode(array("error" => "Error al decodificar el JSON"));
+    } else {
+        $name = $data['name'];
+        $username = $data['username'];
+        $password = $data['password'];
+        $email = $data['email'];
     }
+
     //search if we have results
     $search = $db->prepare('SELECT id, contraseña FROM `usuarios` WHERE usuario = ?');
-    $search->execute([$_POST['username']]);
+    $search->execute([$username]);
     $data = $search->fetch(PDO::FETCH_ASSOC);
     if ($data) {
         echo json_encode($data);
     } else {
         //register user if we haven´t got results 
         $sentencia = $db->prepare("INSERT INTO usuarios (nombre, usuario, contraseña, email) VALUES (?, ?, ?, ?)");
-        $resultado = $sentencia->execute([$_POST['name'],$_POST['username'], $_POST['password'], $_POST['email']]);
+        $resultado = $sentencia->execute([$name, $username, $password, $email]);
         if ($resultado) {
-            $response = array("message" => "Producto insertado correctamente", "id" => $db->lastInsertId());
-            echo json_encode($response);
+            echo json_encode(array("message" => "Registro exitoso"));
         } else {
             echo json_encode(array("message" => "Error al insertar el producto"));
         }
