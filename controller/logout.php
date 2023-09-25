0aa4847c78cc7eed928706a6dd525d400be45592
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2023-09-25 21:10:59 +0200

    logout

--- controller/logout.php
+++ controller/logout.php
@@ -1,4 +1,14 @@
 <?php
+    //CORS, only localhost:4200 and method POST
+    header("Access-Control-Allow-Origin: http://localhost:4200");
+    header("Access-Control-Allow-Methods: GET");
+    header("Access-Control-Allow-Headers: Content-Type");
+    $metodo = $_SERVER["REQUEST_METHOD"];
+    if ($metodo != "GET" && $metodo != "OPTIONS") {
+        exit("Solo se permite método GET");
+    }
     session_start();
     session_destroy();
+    $response = array("message" => "Sesión cerrada");
+    echo json_encode($response);
 ?>
